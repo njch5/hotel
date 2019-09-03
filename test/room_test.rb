f5eb01339857a506e965aa16ef1eba431ffbfb8c
Nicky Choi <nickyjinchoi@gmail.com>
2019-09-03 14:21:06 -0700

    added additional tests for Room class-constructor

--- test/room_test.rb
+++ test/room_test.rb
@@ -10,5 +10,20 @@ describe "Room class" do
     it "is an instance of room" do
       expect(@room).must_be_instance_of Hotel::Room
     end
+
+    it "returns a room id" do
+      expect(@room.id).must_equal 5
+    end
+
+    it "will return an empty array if there are no reservations" do
+      expect(@reservations).must_equal []
+    end
   end
+
+  # describe "Add reservation method" do
+  #   before do
+  #     @room = Hotel::Room.new(id: 8)
+  #     @date_range = Hotel::DateRange.new(start_date: "2019-07-12", end_date: "2019-07-15")
+  #     @reservation = Hotel::Reser
+  # end
 end
