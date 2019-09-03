2e8e4ba715dfc6686c3eec72e628e195d96dcce4
Nicky Choi <nickyjinchoi@gmail.com>
2019-09-03 13:38:26 -0700

    raised an ArgumentError for end_date & added test

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -5,6 +5,8 @@ module Hotel
     def initialize(start_date:, end_date:)
       @start_date = start_date
       @end_date = end_date
+
+      raise ArgumentError, "End date cannot be the same or less than the start date!" if @end_date <= @start_date
     end
   end
 end
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -9,5 +9,11 @@ describe "DateRange Class" do
     it "is an instance of DateRange" do
       expect(@date_range).must_be_kind_of Hotel::DateRange
     end
+
+    it "raises an ArgumentError if end date is less than or equal to start date" do
+      expect do
+        Hotel::DateRange.new("2019-08-25", "2019-08-25")
+      end.must_raise ArgumentError
+    end
   end
 end
