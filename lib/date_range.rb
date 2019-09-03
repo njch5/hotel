ef26e11b53529d7b62db2ecac670c798f8a9c093
Nicky Choi <nickyjinchoi@gmail.com>
2019-09-03 13:47:06 -0700

    created dates & duration methods and tests

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -3,10 +3,18 @@ module Hotel
     attr_reader :start_date, :end_date
 
     def initialize(start_date:, end_date:)
-      @start_date = start_date
-      @end_date = end_date
+      @start_date = Date.parse(start_date)
+      @end_date = Date.parse(end_date)
 
       raise ArgumentError, "End date cannot be the same or less than the start date!" if @end_date <= @start_date
+    end
+
+    def dates
+      return start_date..end_date
+    end
+
+    def duration
+      end_date - start_date
     end
   end
 end
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -16,4 +16,21 @@ describe "DateRange Class" do
       end.must_raise ArgumentError
     end
   end
+
+  describe "Date method" do
+    before do
+      @date_range = Hotel::DateRange.new(start_date: "2019-09-05", end_date: "2019-09-20")
+
+      @start_date = Date.parse("2019-09-05")
+      @end_date = Date.parse("2019-09-20")
+    end
+
+    it "returns instances of Date" do
+      expect(@date_range.dates).must_equal (@start_date..@end_date)
+    end
+
+    it "returns the duration of stays (number of nights)" do
+      expect(@date_range.duration).must_equal 15
+    end
+  end
 end
