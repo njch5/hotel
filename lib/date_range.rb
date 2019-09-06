8d62783c210dbc72e4193b0822266efd3674127c
Nicky Choi <nickyjinchoi@gmail.com>
2019-09-05 19:10:43 -0700

    added additional tests for date_range & hotel_sys

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -8,7 +8,9 @@ module Hotel
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
 
-      raise ArgumentError, "End date cannot be the same or less than the start date!" if @end_date <= @start_date
+      raise ArgumentError,
+            "End date cannot be the same or less than the start date!
+            " if @end_date <= @start_date || @end_date == nil && @start_date == nil
     end
 
     def dates
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -10,14 +10,34 @@ describe "DateRange Class" do
       expect(@date_range).must_be_kind_of Hotel::DateRange
     end
 
-    it "raises an ArgumentError if end date is less than or equal to start date" do
+    it "takes a start and end date" do
+      expect(@date_range).must_respond_to :start_date
+      expect(@date_range.start_date).must_equal Date.parse("2019-08-25")
+
+      expect(@date_range).must_respond_to :end_date
+      expect(@date_range.end_date).must_equal Date.parse("2019-08-30")
+    end
+
+    it "raises an ArgumentError if end date is equal to start date" do
       expect do
         Hotel::DateRange.new("2019-08-25", "2019-08-25")
       end.must_raise ArgumentError
     end
+
+    it "raises an ArgumentError if end date is before start date" do
+      expect do
+        Hotel::DateRange.new(start_date: "2019-08-20", end_date: "2019-08-10")
+      end.must_raise ArgumentError
+    end
+
+    it "raises an ArgumentError if no dates are given" do
+      expect do
+        Hotel::DateRange.new(start_date: "", end_date: "")
+      end.must_raise ArgumentError
+    end
   end
 
-  describe "Date method" do
+  describe "Dates method" do
     before do
       @date_range = Hotel::DateRange.new(start_date: "2019-09-05", end_date: "2019-09-20")
 
@@ -35,7 +55,7 @@ describe "DateRange Class" do
   end
 
   describe "Overlap? method" do
-    it "will returns true if two date ranges overlap" do
+    it "will return true if two date ranges overlap" do
       @date_range_one = Hotel::DateRange.new(start_date: "2019-07-15", end_date: "2019-07-30")
       @date_range_two = Hotel::DateRange.new(start_date: "2019-07-05", end_date: "2019-07-16")
 
@@ -47,6 +67,27 @@ describe "DateRange Class" do
       @date_range_two = Hotel::DateRange.new(start_date: "2019-06-05", end_date: "2019-06-16")
 
       expect(@date_range_one.overlap?(@date_range_two)).must_equal false
+    end
+
+    it "will return false if one date range's start date is on the same day as the other date range's end date" do
+      @date_range_one = Hotel::DateRange.new(start_date: "2019-10-05", end_date: "2019-10-30")
+      @date_range_two = Hotel::DateRange.new(start_date: "2019-10-30", end_date: "2019-11-05")
+
+      expect(@date_range_one.overlap?(@date_range_two)).must_equal false
+    end
+
+    it "will return true if one date range is within another date range" do
+      @date_range_one = Hotel::DateRange.new(start_date: "2019-02-10", end_date: "2019-02-28")
+      @date_range_two = Hotel::DateRange.new(start_date: "2019-02-15", end_date: "2019-02-18")
+
+      expect(@date_range_one.overlap?(@date_range_two)).must_equal true
+    end
+
+    it "will return true when one date range starts before the other date range begins" do
+      @date_range_one = Hotel::DateRange.new(start_date: "2019-06-12", end_date: "2019-06-18")
+      @date_range_two = Hotel::DateRange.new(start_date: "2019-06-10", end_date: "2019-06-30")
+
+      expect(@date_range_one.overlap?(@date_range_two)).must_equal true
     end
   end
 end
